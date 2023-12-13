168a67d3ebffbb7764cbecce669a2da24a8b5291
Brian Bud <hello@brianbud.com>
2023-12-12 20:41:21 -0500

    recursive

--- mergeSort.js
+++ mergeSort.js
@@ -6,6 +6,8 @@ function mergeSort(arr) {
   const mid = Math.floor(arr.length / 2);
   const left = arr.slice(0, mid);
   const right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
