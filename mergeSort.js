9c3d02507f64bbf6b20bdc7de2deb0de8f3b2631
Brian Bud <hello@brianbud.com>
2023-12-12 20:21:41 -0500

    divide array in half

--- mergeSort.js
+++ mergeSort.js
@@ -2,4 +2,8 @@ function mergeSort(arr) {
   if (arr.length <= 1) {
     return arr;
   }
+
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
 }
