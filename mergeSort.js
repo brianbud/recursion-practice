e67390f351f8a3f96f56ec3709a46691d8e90741
Brian Bud <hello@brianbud.com>
2023-12-12 20:37:08 -0500

    merge function

--- mergeSort.js
+++ mergeSort.js
@@ -7,3 +7,17 @@ function mergeSort(arr) {
   const left = arr.slice(0, mid);
   const right = arr.slice(mid);
 }
+
+function merge(left, right) {
+  let sortedArr = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      sortedArr.push(left.shift());
+    } else {
+      sortedArr.push(right.shift());
+    }
+  }
+
+  return [...sortedArr, ...left, ...right];
+}
